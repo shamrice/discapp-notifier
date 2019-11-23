3c5dd301ea3d45ae6736ffef40fe9cca3b0898b6
shamrice <shamrice@gmail.com>
2019-11-23 02:07:12 -0500

    Initial commit of project

---
+++ src/main/java/io/github/shamrice/discapp/notification/Application.java
@@ -0,0 +1,12 @@
+package io.github.shamrice.discapp.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
